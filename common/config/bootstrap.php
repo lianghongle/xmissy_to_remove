d5c5905f1ade59bbde6e41484025413b0405ffb6
神圣小树 <lianghogle@163.com>
2018-09-05 17:49:21 +0800

    Initial commit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@strong', dirname(dirname(__DIR__)) . '/strong');
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/app_frontend');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/app_backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/app_console');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/app_api');
